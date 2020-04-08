299d0878e0cc5deb89c501b1fa9a9fe1a3f67b63
Gordon Lim <honwei189@gmail.com>
2020-04-08 15:05:03 +0800

    Added setup docker script

---
+++ CentOS-8/setup-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+###
+ # @description       : Docker installation script
+ # @version           : "1.0.0" 
+ # @creator           : Gordon Lim <honwei189@gmail.com>
+ # @created           : 08/04/2020 12:05:59
+ # @last modified     : 08/04/2020 12:06:36
+ # @last modified by  : Gordon Lim <honwei189@gmail.com>
+ ###
+ 
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+
+#For CentOS 8.1
+dnf clean all 
+dnf -y remove podman
+#dnf -y install oci-systemd-hook libvarlink
+#rpm -Uvh --nodeps $(repoquery --location podman)
+dnf install -y @container-tools
+
+dnf install docker-ce --nobest -y
+#dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.10-3.2.el7.x86_64.rpm -y
+#dnf install docker-ce -y
+
+systemctl enable --now docker
+systemctl status docker
