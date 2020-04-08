e5e0f74d46560d37dc97508f5922d5ea46c47a72
Gordon Lim <honwei189@gmail.com>
2020-04-08 16:54:59 +0800

    Added a script to renew whitelist for NginX

---
+++ utilities/renew_nginx_whitelist.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+###
+ # @description       : Renew whitelist for NginX
+ # @version           : "1.0.0" 
+ # @creator           : Gordon Lim <honwei189@gmail.com>
+ # @created           : 08/04/2020 16:50:52
+ # @last modified     : 08/04/2020 16:54:28
+ # @last modified by  : Gordon Lim <honwei189@gmail.com>
+ ###
+
+#nginx config
+#e.g:
+#    location / {
+#        include /etc/nginx/conf.d/server/whitelist.conf;
+#    }
+
+################################################################
+# Hostname allows to access
+################################################################
+hosts="abc.noip.info
+abc.example.com"
+
+################################################################
+# IPs allows to access
+################################################################
+ips="192.168.1.0/24
+172.1.1.1"
+
+################################################################
+# Whitelist config file  (DO NOT CHANGE)
+################################################################
+whitelist="/etc/nginx/whitelist.conf"
+
+
+##########
+
+cat /dev/null > $whitelist
+for ip in $ips
+do
+    echo "      allow $ip;" >> $whitelist
+done
+
+for host in $hosts
+do
+  ip=$(php -r "echo gethostbyname('${host}');")
+  echo "      allow $ip;" >> $whitelist
+done
+
+
+#for ip in $(curl --silent https://www.cloudflare.com/ips-v4)
+#do
+#    echo "      allow $ip;" >> $whitelist
+#done
+
+#for ip in $(curl --silent https://www.cloudflare.com/ips-v6)
+#do
+#    echo "      allow $ip;" >> $whitelist
+#done
+
+echo "      deny all;" >> $whitelist
+
+nginx -s reload
