c683a0707f032e222edab249df89cb8cefc26b17
Gordon Lim <honwei189@gmail.com>
2020-04-30 16:55:41 +0800

    Add dynamic IP or fixed IP address to NGINX to grant certains website access

--- utilities/renew_nginx_whitelist_map.sh
+++ utilities/renew_nginx_whitelist_map.sh
@@ -6,7 +6,7 @@
 # @version           : "1.0.0"
 # @creator           : Gordon Lim <honwei189@gmail.com>
 # @created           : 14/04/2020 13:43:30
- # @last modified     : 14/04/2020 14:18:13
+ # @last modified     : 28/04/2020 13:26:57
 # @last modified by  : Gordon Lim <honwei189@gmail.com>
 ###
 
@@ -34,17 +34,22 @@ ips="192.168.1.0/24
 ################################################################
 # Whitelist config file  (DO NOT CHANGE)
 ################################################################
-whitelist="/etc/nginx/conf.d/whitelist.conf"
+whitelist="/etc/nginx/conf.d/whitelist.conf"  #auto load conf.d/*.conf by nginx
 check_white_ip="/etc/nginx/conf.d/server/check_white_ip.conf"
 
 ##########
 
-cat /dev/null >$whitelist
+if [ -f $whitelist ];
+then
+    cat /dev/null > $whitelist
+else
+    touch $whitelist
+fi
 
 #echo "  #set \$realip \$remote_addr;" >> $whitelist
 #echo "  #if (\$http_x_forwarded_for ~ \"^(\d+\.\d+\.\d+\.\d+)\") {" >> $whitelist
 #echo "  #   set \$realip \$1;" >> $whitelist
-#echo "  #}" >> \$whitelist
+#echo "  #}" >> $whitelist
 #echo "  " >> $whitelist
 #echo "  #fastcgi_param REMOTE_ADDR \$realip;" >> $whitelist
 #echo "" >> $whitelist
@@ -52,6 +57,11 @@ cat /dev/null >$whitelist
 echo "  map \$http_x_forwarded_for \$real_ip {" >>$whitelist
 echo "      ~^(\d+\.\d+\.\d+\.\d+) \$1;" >>$whitelist
 echo "      default \$remote_addr;" >>$whitelist
+echo "  }" >>$whitelist
+
+echo "" >>$whitelist
+echo "  map \$proxy_add_x_forwarded_for \$real_ip {" >>$whitelist
+echo "      \"~(?<IP>([0-9]{1,3}\.){3}[0-9]{1,3}),.*\" \$IP;" >>$whitelist
 echo "  }" >>$whitelist
 
 echo "" >>$whitelist
@@ -94,4 +104,5 @@ if [ ! -f $check_white_ip ]; then
     echo "        }" >>$check_white_ip
 fi
 
-nginx -s reload
+#nginx -s reload
+service nginx reload
