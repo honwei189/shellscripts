25057f1e3f42d3eaeb962a4c4f3dfd0b09928ec5
Gordon <59283290+honwei189@users.noreply.github.com>
2024-10-08 10:37:18 +0800

    Add install docker-compose

--- CentOS-8/setup-docker.sh
+++ CentOS-8/setup-docker.sh
@@ -24,3 +24,9 @@ dnf install docker-ce --allowerasing -y
 
 systemctl enable --now docker
 systemctl status docker
+
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/2.23.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
